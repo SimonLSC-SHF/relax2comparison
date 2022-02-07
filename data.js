b3cb0a72c502207fd8015bea6797251439be9ef6
SimonLSC-SHF <96471303+SimonLSC-SHF@users.noreply.github.com>
2022-02-07 16:57:39 +0000

    first commit

---
+++ data.js
@@ -0,0 +1,527 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-relax---hbhc_1",
+      "name": "Relax - HBHC_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.01998258727086366,
+          "pitch": 0.1498373126124477,
+          "rotation": 0,
+          "target": "1-relax---hbhc_2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-relax---hbhc_2",
+      "name": "Relax - HBHC_2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.397291787303857e-8,
+          "pitch": 0.1520375634170481,
+          "rotation": 0,
+          "target": "2-relax---hbhc_3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-relax---hbhc_3",
+      "name": "Relax - HBHC_3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.013322678938013865,
+          "pitch": 0.20788856668097466,
+          "rotation": 0,
+          "target": "3-relax---hbhc_4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-relax---hbhc_4",
+      "name": "Relax - HBHC_4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-relax---hblc_1",
+      "name": "Relax - HBLC_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.015542811685087088,
+          "pitch": 0.14767764647126214,
+          "rotation": 0,
+          "target": "5-relax---hblc_2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-relax---hblc_2",
+      "name": "Relax - HBLC_2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.004441140823036704,
+          "pitch": 0.18440737593052958,
+          "rotation": 0,
+          "target": "6-relax---hblc_3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-relax---hblc_3",
+      "name": "Relax - HBLC_3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.017762780037593018,
+          "pitch": 0.216361724029138,
+          "rotation": 0,
+          "target": "11-relax---lbhc_4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-relax---hblc_4",
+      "name": "Relax - HBLC_4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-relax---lbhc_1",
+      "name": "Relax - LBHC_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0050736772701345245,
+          "pitch": 0.1606136816223298,
+          "rotation": 0,
+          "target": "9-relax---lbhc_2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-relax---lbhc_2",
+      "name": "Relax - LBHC_2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.008882105529995243,
+          "pitch": 0.17795674523757476,
+          "rotation": 0,
+          "target": "10-relax---lbhc_3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-relax---lbhc_3",
+      "name": "Relax - LBHC_3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.004441140823036704,
+          "pitch": 0.17796220273903174,
+          "rotation": 0,
+          "target": "11-relax---lbhc_4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-relax---lbhc_4",
+      "name": "Relax - LBHC_4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-relax---lblc_1",
+      "name": "Relax - LBLC_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.005568156612699937,
+          "pitch": 0.1504289523149236,
+          "rotation": 0,
+          "target": "13-relax---lblc_2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-relax---lblc_2",
+      "name": "Relax - LBLC_2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.004441138960428859,
+          "pitch": 0.17150186367981846,
+          "rotation": 0,
+          "target": "14-relax---lblc_3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-relax---lblc_3",
+      "name": "Relax - LBLC_3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.017762768865249967,
+          "pitch": 0.20999937094905263,
+          "rotation": 0,
+          "target": "15-relax---lblc_4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "15-relax---lblc_4",
+      "name": "Relax - LBLC_4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Relax comparison",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
